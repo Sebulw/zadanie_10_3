71d88f79d6f7ca0cddc15ee157f6301164c7be2b
Sebastian Wojnicz <sebulw@gmail.com>
2017-10-04 13:04:44 +0200

    carousel script done, requires more work for new features

--- workspace/js/scripts.js
+++ workspace/js/scripts.js
@@ -1,15 +1,14 @@
 $(function() {
-	// this code will execute after the DOM is loaded
 	var carouselList = $("#carousel ul");
-	setInterval(changeSlide, 3000);
+	setInterval(changeSlide, 5000);
 	function changeSlide() {
-		carouselList.animate({'marginLeft':-400}, 500, moveFirstSlide);
-	};
+		carouselList.animate({'marginLeft':-640}, 1000, moveFirstSlide);
+	}
 	changeSlide();
 	function moveFirstSlide() {
 		var firstItem = carouselList.find('li:first');
 		var lastItem = carouselList.find('li:last');
 		lastItem.after(firstItem);
 		carouselList.css({marginLeft:0});
-	};
+	}
 });
